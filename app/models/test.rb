6e6c53b7954230ed952945d7069d7f113b45c3c7
h0qw010xz <CloserCloseCloser@gmail.com>
2019-01-10 14:36:48 -0500

    add result

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :question
+  has_many :results
+
+  def self.get_by_category(c)
+    Test.joins('JOIN categories ON tests.category_id = categories.id')
+        .where(categories: {title: c}).pluck(:title).sort
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ruby = Category.create(title: 'Ruby')
+html = Category.create(title: 'HTML')
+css = Category.create(title: 'CSS')
+
+user_names = %w[Jenifer Arron Abe Max]
+users = user_names.reduce([]) { |users, name| users << User.create(name: name) }
+
+tests = Test.create([{ title: 'Ruby basics', level: 0, category_id: ruby.id },
+                     { title: 'Ruby advanced', level: 1, category_id: ruby.id },
+                     { title: 'Ruby core', level: 2, category_id: ruby.id },
+                     { title: 'HTML basics', level: 0, category_id: html.id },
+                     { title: 'HTML forms', level: 0, category_id: html.id },
+                     { title: 'HTML canvas', level: 1, category_id: html.id },
+                     { title: 'CSS basics', level: 0, category_id: css.id },
+                     { title: 'CSS flexbox', level: 1, category_id: css.id },
+                     { title: 'CSS grid', level: 1, category_id: css.id }])
+
+QUESTIONS = 4
+ANSWERS = 4
+
+tests.each do |test|
+  QUESTIONS.times do |i|
+    q = Question.create(body: "#{test.title}, question #{i}", test_id: test.id)
+    ANSWERS.times do |j|
+      correct = j == 1
+      Answer.create(body: "Its a #{correct} answer for #{test.title}",
+                    correct: correct,
+                    question_id: q.id)
+    end
+  end
+end
+
+users.each do |user|
+  tests.sample(4).each do |test|
+    Result.create(score: rand(1..4), test_id: test.id, user_id: user.id)
+  end
+end
