6bef862d34f9c125bb6afa1c1608679f0d963b6e
h0qw010xz <CloserCloseCloser@gmail.com>
2019-01-12 16:34:04 -0500

    fix user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :results
   has_many :tests, through: :results, dependent: :destroy
-  has_many :creator, class_name: 'Test',foreign_key: 'author_id', dependent: :destroy
+  has_many :created_tests, class_name: 'Test',foreign_key: 'author_id', dependent: :destroy
 
   def tests_by_level(level)
     tests.where(level: level)
