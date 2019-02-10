680be617202106ca8d663fc3955d309ca919b2a5
h0qw010xz <CloserCloseCloser@gmail.com>
2019-02-10 12:23:27 -0500

    fix minor errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages, dependent: :destroy
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
-  validates :email, presence: true, uniqueness: true, format: { with: /.+@.+\..+/i }
   validates :password, length: { maximum: 20, minimum: 6 }, confirmation: true
 
   def by_level(level)
