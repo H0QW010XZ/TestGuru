3b57b3d75425301a41b70270ed25934dbfe24010
h0qw010xz <CloserCloseCloser@gmail.com>
2019-02-12 14:19:07 -0500

    fix minor errors

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-   config.i18n.available_locales = :ru
+   config.i18n.available_locales = %i[ru en]
    config.time_zone = 'Moscow'
    config.i18n.default_locale = :ru
 
