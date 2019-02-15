82dafe560859f80d0d990cdc2f6efbef576b705e
h0qw010xz <CloserCloseCloser@gmail.com>
2019-02-15 14:39:54 -0500

    add first styles(bootstrap)

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -2,9 +2,4 @@ module TestsHelper
   def github_url(author, repo)
     "https://github.com/#{author}/#{repo}"
   end
-
-  def test_header(test)
-    action = test.new_record? ? t('helpers.admin_tests.create') : t('helpers.admin_tests.edit')
-    "#{action} #{t('helpers.admin_tests.test')} «#{test.title}»"
-  end
 end
