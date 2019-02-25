67836da5dfc878b6524817d3d96dd1ffba6357ca
h0qw010xz <CloserCloseCloser@gmail.com>
2019-02-25 13:59:36 -0500

    add pass check

--- app/assets/javascripts/utilities/passwords.js
+++ app/assets/javascripts/utilities/passwords.js
@@ -1,20 +1,19 @@
-var password;
-var password_confirm;
-
 document.addEventListener('turbolinks:load', function () {
-    password = document.querySelector('.password_field');
-    password_confirm = document.querySelector('.password_confirmation_field');
+    var password = document.querySelector('#user_password')
+    var password_confirmation = document.querySelector('#user_password_confirmation')
 
-    if (password && password_confirm) {
-        password.addEventListener('input',  password_comparison);
-        password_confirm.addEventListener('input', password_comparison)};
-});
+    if (password && password_confirmation) {
+        password.addEventListener('input', password_comparison(password, password_confirmation))
 
-function password_comparison() {
-    if (password.value === password_confirm.value && (password.value != "")) {
+        password_confirmation.addEventListener('input', password_comparison(password, password_confirmation))
+    }
+})
+
+function password_comparison(password, password_confirmation) {
+    if (password.value === password_confirmation.value && (password.value != "")) {
         document.querySelector('.octicon-check').classList.remove('hide');
         document.querySelector('.octicon-alert').classList.add('hide');
-    } else if (password_confirm.value === "") {
+    } else if (password_confirmation.value === "") {
         document.querySelector('.octicon-check').classList.add('hide');
         document.querySelector('.octicon-alert').classList.add('hide');
     } else {
