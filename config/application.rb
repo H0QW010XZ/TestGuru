349c22c7271a256928cbc89c8dddfff736a0e0cf
h0qw010xz <CloserCloseCloser@gmail.com>
2019-03-08 04:04:54 -0500

    fix dotenrails

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,10 @@ module TestGuru
     config.time_zone = 'Moscow'
     config.i18n.default_locale = :ru
 
-    Dotenv::Railtie.load
+    if ['development', 'test'].include? ENV['RAILS_ENV']
+      Dotenv::Railtie.load
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
